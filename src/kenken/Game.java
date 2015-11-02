effff3a958f7c667724eb6fb1ac5d9ab8cef1e7c
pol <pol@pol>
2015-11-02 17:24:04 +0100

    preliminar de la clase Game

---
+++ src/kenken/Game.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kenken;
+
+/**
+ *
+ * @author pol
+ */
+public class Game {
+    
+    enum Mode{easy,medium,hard};
+    
+    private long time;
+    private Mode mode;
+
+    public Game(Mode mode)
+    {
+        time = System.nanoTime();
+        this.mode = mode;
+    }
+    
+    public long getTime()
+    {
+        return System.nanoTime() - time;
+    }
+    
+    public Mode getMode()
+    {
+        return mode;
+    }
+}
