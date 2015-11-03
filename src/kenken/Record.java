cbe25c238375be3cd84493fe474b7cfd443cdd06
marc <marc@Linux-Sobretaula>
2015-11-03 22:56:23 +0100

    Inital Commit

---
+++ src/kenken/Record.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kenken;
+
+import java.time.Duration;
+
+/**
+ *
+ * @author Marc Ferré Monné
+ */
+public class Record {
+    
+    private String namePlayer;
+    private Duration time;
+    
+}
