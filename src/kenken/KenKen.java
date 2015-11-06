5f229614f56c1f3269f0be89b20f419edc7c0f8e
Alberto Lopez Sanchez <alberto.lopez.sanchez@est.fib.upc.edu>
2015-11-06 13:53:01 +0100

    Creacion de un Junit sin probar de UserDBController

--- src/kenken/KenKen.java
+++ src/kenken/KenKen.java
@@ -19,55 +19,11 @@ public class KenKen {
     /**
      * @param args the command line arguments
      */
-    UserDBController db = new UserDBController();
+    
     
     
     public KenKen() {
-        UserKenKen marc = new UserKenKen("Marc", "12345");
         
-        marc.setKekeniadas(133);
-        marc.setTablerosCreados(43);
-        db.createUser(marc);
-        if (db.createUser(marc) == -1) {
-            System.err.println("Error al crear usuario: Usuario ya existente.");
-        }
-        
-        marc.setTablerosCreados(60);
-        if (db.modifyUser(marc, null) == 0) {
-            System.out.println("Usuario modificado correctamente.");
-        }
-        
-        if (db.modifyUser(marc, "asdfwsdf") != 0) {
-            System.err.println("Error al modificar usuario : El anterior no existe.");
-        }
-        
-        if (db.modifyUser(marc, "Marc") != 0) {
-            System.err.println("Error al modificar usuario : El anterior no existe.");
-        }
-        
-        marc.setUsername("rodolfo");
-        if (db.modifyUser(marc, "Marc") != 0) {
-            System.err.println("Error al modificar usuario: rodolfo ya existe.");
-        }
-                
-        
-        UserKenKen marc2;
-        try {
-            marc2 = (UserKenKen) db.getUser("alsdkfj");
-            
-            System.out.printf("%s, Kenkeniadas: %d, TablerosCreados: %d", 
-                marc2.getUsername(), marc2.getKekeniadas(), marc2.getTablerosCreados());
-            
-        } catch (IOException ex) {
-            System.err.println("El usuario no existe o no se puede acceder a disco.");
-        }
-        
-        db.deleteUser("Marc");
-        db.deleteUser("rodolfo");
-        
-        if(db.deleteUser("manuala") != 0) {
-            System.err.println("Error al borrar el usuario.");
-        }
                
     }
     
