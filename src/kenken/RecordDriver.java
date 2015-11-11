149d121a3ec96717d64bfd0de4c9bfeb245eae09
Marc <Marc@W-Sobretaula>
2015-11-11 20:09:59 +0100

    Els parses funcionen correctament.

--- src/kenken/RecordDriver.java
+++ src/kenken/RecordDriver.java
@@ -15,7 +15,16 @@ import java.util.ArrayList;
 public class RecordDriver {
     public static void main (String[] args){
         Record testRecord = new Record("Marc", 60);
-        String testString = testRecord.getTimeAsString();
-        System.out.println(testString);
+        ArrayList<String> test = testRecord.recordToString();
+        int size = test.size();
+        for (int i = 0; i < size; i++){
+            System.out.println(test.get(i));
+        }
+        System.out.println("Actualment llegit del vector de Strings.");
+        testRecord = Record.stringToRecord(test);
+        System.out.println(testRecord.getNamePlayer());
+        System.out.println(testRecord.getTime().getSeconds());
+        System.out.println("Actualment llegit de l'objecte Record.");
+        
     }
 }
