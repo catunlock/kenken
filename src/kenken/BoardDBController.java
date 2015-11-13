1d45f71cc88d288520d2971e2e4215e1b7af9546
asus <asus@asuspc>
2015-11-13 12:22:50 +0100

    Empiezo a hacer BoardDBController

--- src/kenken/BoardDBController.java
+++ src/kenken/BoardDBController.java
@@ -5,6 +5,12 @@
  */
 package kenken;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author joan.pol.alejandre
@@ -25,16 +31,39 @@ public class BoardDBController {
     */
     
     public int createBoard(Board newBoard){
-        int result = 1;
+        int result = 0;
+        int resultInfo = 0;
+        int resultObj = 0;
         String ubicacio = getPath(newBoard);
                 
                 /* tamaño tablero, persona que lo ha creado, */
         
-        String fisica = ubicacio+ExtensionFisica;
-        String info = ubicacio+ExtensionInfo;
+        String pathFisica = ubicacio+ExtensionFisica;
+        String pathInfo = ubicacio+ExtensionInfo;
         
+        if (new File(pathInfo).isFile() || new File(pathFisica).isFile()) {
+            result = -1;
+        }
+        else{
+            /* info de tablero contiene: nombre tablero, persona que lo ha creado, dificultad, tamX, tamY */
+            ArrayList<String> infoBoard = new ArrayList<String>();
+            infoBoard.add(newBoard.getBoardName());
+            infoBoard.add(newBoard.getUsername());
+            infoBoard.add(newBoard.getDifficulty());
+            infoBoard.add(String.valueOf(newBoard.getSizeX()));
+            infoBoard.add(String.valueOf(newBoard.getSizeY()));
+            
+            //crear el fichero de info y fisico
+            resultInfo = writeBoardInfo(newBoard.getBoardName(), infoBoard, pathInfo);
+            resultObj = writeBoardObj(newBoard, pathFisica);
+            
+            //comprobar errores
+            if (resultInfo == -2 || resultObj == -2){
+                result = -2;
+            }
+        }
+
         return result;
-        
     }
                         
     private String getPath(Board board){
@@ -43,6 +72,28 @@ public class BoardDBController {
     
     private String getPath(String boardName){
         return Directory+boardName;
+    }
+
+    private int writeBoardInfo(String boardName, ArrayList<String> infoBoard, String infoPath) {
+        int result = -2;
+        
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(infoPath);
+            
+            result = 0;
+        } catch (FileNotFoundException ex) {
+            
+        } catch (IOException ex) {
+            
+        }
+        
+        
+        return result;
+    }
+
+    private int writeBoardObj(Board newBoard, String fisica) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
