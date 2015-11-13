ce809f02649a56e4ec09c116f84bd4bc5bea19ab
joan.pol.alejandre <joan.pol.alejandre@a5s102pc51.fib.upc.es>
2015-11-13 20:47:52 +0100

    BoardDBControllerDriver CASI acabado

--- src/kenken/BoardDBControllerDriver.java
+++ src/kenken/BoardDBControllerDriver.java
@@ -16,70 +16,82 @@ public class BoardDBControllerDriver {
     public static void main(String[] args) throws IOException
     {
         BoardDBController test = new BoardDBController();
-        
-        Board board = new Board(5,5);
-        board.setBoardName("prueba");
-        board.setDifficulty("chungo");
-        board.setUsername("manolo");
-        //System.out.println(test.createBoard(board));
-        //System.out.println(test.deleteBoard("prueba"));
-        Board cargada = test.loadBoard("prueba");
-        if (cargada != null){
-            System.out.println("Board Name: " + cargada.getBoardName());
-            System.out.println("Usuari Board: " + cargada.getUsername());
-            System.out.println("Dificultat: " + cargada.getDifficulty());
-            System.out.println("Size X: " + cargada.getSizeX());
-            System.out.println("Size Y: " + cargada.getSizeY());
-        }
-        else{
-            System.out.println("No existeix aquesta Board");
-        }
-
-        
-        /*Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
         System.out.println("-----------------------------------------------");
-        System.out.println("Provant UserDBController");
+        System.out.println("Provant BoardDBController");
         System.out.println("-----------------------------------------------");
         System.out.println("Introdueix un numero per a executar la operació:");
         System.out.println("-----------------------------------------------"); 
-        System.out.println("1. Crear nou board --> 0: creat correctament, -1: usuari existent, -2: error intern");
-        System.out.println("2. Modificar usuari --> 0: correcte, -1: usuari existent, -2: error intern");
-        System.out.println("3. Canviar nom d'usuari");
-        System.out.println("4. Canviar contrasenya");
-        System.out.println("5. Incrementar SolvedGames");
-        System.out.println("6. Incrementar StartedGames");
-        System.out.println("7. Incrementar TotalCreatedBoards");
-        System.out.println("8. Incrementar TotalTimePlayed");
+        System.out.println("1. Crear nova Board a la DB --> 0: creat correctament, -1: ja existent, -2: error intern");
+        System.out.println("2. Eliminar Board de la DB --> 0: correcte, -1: Board no existeix, -2: error intern");
+        System.out.println("3. Carregar Board de la DB i mostrar informació");
         System.out.println("-1. Exit");
         System.out.println("-----------------------------------------------");
         
         Integer option;
-        String user;
-        String pass;
+        String boardName;
+        int result;
+        String enter;
         
         while((option = sc.nextInt()) != -1) {
-            String enter = sc.nextLine();
+            enter = sc.nextLine();
             switch(option){
-                case 1: 
-                    System.out.println("Nom d'usuari:");
-                    user = sc.nextLine();
-                    System.out.println("Contrasenya d'usuari:");
-                    pass = sc.nextLine();
-                    User usuari = new User(user,pass);
-                    Integer resultat = test.createUser(usuari);
-                    System.out.println(resultat);
+                case 1:
+                    System.out.println("Introdueix nom de tauler:");
+                    boardName = sc.nextLine();
+                    System.out.println("Introdueix tamany tauler:");
+                    int x = sc.nextInt();
+                    enter = sc.nextLine();
+                    System.out.println("Introdueix usuari de tauler:");
+                    String userBoard = sc.nextLine();
+                    System.out.println("Introdueix dificultat de tauler:");
+                    String difficulty = sc.nextLine();
+                    Board board = new Board(x,x);
+                    board.setBoardName(boardName);
+                    board.setDifficulty(difficulty);
+                    board.setUsername(userBoard);
+                    result = test.createBoard(board);
+                    if (result == 0){
+                        System.out.println("Tauler creat correctament");
+                    }
+                    else if (result == -1){
+                        System.out.println("Nom de tauler ja existent");
+                    }
+                    else{
+                        System.out.println("Error intern");
+                    }
                     break;
                 case 2:
-                    System.out.println("Nom usuari a modificar:");
-                    user = sc.nextLine();
-                    System.out.println("Nova contrasenya:");
-                    pass = "";
-                    User usuari2 = new User(user,pass);
-                    test.modifyUser(usuari2,user);
+                    System.out.println("Introdueix nom de tauler a eliminar:");
+                    boardName = sc.nextLine();
+                    boardName = boardName.replaceAll("(\\r|\\n)", "");
+                    result = test.deleteBoard(boardName);
+                    if (result == 0){
+                        System.out.println("Tauler eliminat correctament");
+                    }
+                    else if (result == -1){
+                        System.out.println("Nom de tauler inexistent");
+                    }
+                    else{
+                        System.out.println("Error intern");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Introdueix nom de tauler a carregar:");
+                    boardName = sc.nextLine();
+                    Board b = test.loadBoard(boardName);
+                    if(b == null){
+                        System.out.println("Board no existeix.");
+                    }
+                    else{
+                        System.out.println("Nom tauler carregat: " + b.getBoardName());
+                        System.out.println("User del tauler carregat: " + b.getUsername());
+                        System.out.println("Dificultat del tauler carregat: " + b.getDifficulty());
+                        System.out.println("Mida del tauler carregat: " + b.getSizeX());
+                    }
                     break;
             }
-        }*/
-    }
-    
+        }
+    }   
 }
