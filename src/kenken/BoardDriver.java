a0b4dac376e76ebde4313f9b07c4a9467a92df4d
Gerard <gerard.otero@est.fib.upc.edu>
2015-11-15 20:33:30 +0100

    Hecho

--- src/kenken/BoardDriver.java
+++ src/kenken/BoardDriver.java
@@ -15,79 +15,69 @@ import java.util.Scanner;
 public class BoardDriver {
     public static void main(String[] args) throws IOException
     {
-        Cell test;
+        Board test;
+        Region r;
         Scanner sc = new Scanner(System.in);
-        int posX, posY, solution, option;
+        int size, option;
+        int posX, posY, solution;
         boolean original;
         
         System.out.println("-----------------------------------------------");
-        System.out.println("Provant Cell");
+        System.out.println("Provant Board");
         System.out.println("-----------------------------------------------");
-        System.out.println("Introdueix posició X de la Cell:");
-        posX = sc.nextInt();
-        System.out.println("Introdueix posició Y de la Cell:");
-        posY = sc.nextInt();
-        System.out.println("Introdueix valor de la solució de la Cell");
-        solution = sc.nextInt();
-        System.out.println("Introdueix si la Cell és original:");
-        System.out.println("1. Sí");
-        System.out.println("2. No");
-        if (sc.nextInt() == 1) original = true;
-        else original = false;
-        test = new Cell(posX, posY, original, solution);
+        System.out.println("Introdueix size del Board:");
+        size = sc.nextInt();
+        test = new Board(size);
         System.out.println("Introdueix un numero per a executar la operació:");
         System.out.println("-----------------------------------------------"); 
-        System.out.println("1. Mostra posició X de la Cell");
-        System.out.println("2. Mostra posició Y de la Cell");
-        System.out.println("3. Mostra el valor correcte de la Cell");
-        System.out.println("4. Dona un valor a la cell");
-        System.out.println("5. Mostra el valor que li dona l'usuari a la Cell");
-        System.out.println("6. Mostra si la cell és original");
-        System.out.println("7. Mostra si el valor que l'usuari li dona a la Cell és el correcte");
-        System.out.println("8. Afegeix un valor a la llista de valors possibles de la Cell");
-        System.out.println("9. Elimina un valor de la llista de valors possibles de la Cell");
-        System.out.println("10. Mostra el valors possibles de la Cell");
-        System.out.println("11. Mostra la size de la llista de valors possibles de la Cell");
+        System.out.println("1. Dona nom a la Board");
+        System.out.println("2. Dona el nom de l'usuari creador de la Board");
+        System.out.println("3. Mostra el nom de la Board");
+        System.out.println("4. Mostra el nom del creador de la Board");
+        System.out.println("5. Afegeix una cell");
+        System.out.println("6. Mostra una Cell");
         System.out.println("-1. Exit");
         System.out.println("-----------------------------------------------");
         
         while((option = sc.nextInt()) != -1) {
             switch(option){
                 case 1:
-                    System.out.println(test.getPosX());
+                    System.out.print("Dona nom a la Board:");
+                    test.setBoardName(sc.next());
                     break;
                 case 2:
-                    System.out.println(test.getPosY());
+                    System.out.print("Dona nom del creador de la board:");
+                    test.setUsername(sc.next());
                     break;
                 case 3:
-                    System.out.println(test.getSolutionValue());
+                    System.out.print("Nom de la Board:");
+                    System.out.println(test.getBoardName());
                     break;
                 case 4:
-                    System.out.print("Valor:");
-                    test.setUserValue(sc.nextInt());
+                    System.out.print("Creador:");
+                    System.out.println(test.getUsername());
                     break;
                 case 5:
-                    System.out.println(test.getUserValue());
+                    System.out.println("Introdueix posició X de la Cell:");
+                    posX = sc.nextInt();
+                    System.out.println("Introdueix posició Y de la Cell:");
+                    posY = sc.nextInt();
+                    System.out.println("Introdueix valor de la solució de la Cell");
+                    solution = sc.nextInt();
+                    System.out.println("Introdueix si la Cell és original (1 = sí, 2 = no):");
+                    if (sc.nextInt() == 1) original = true;
+                    else original = false;
+                    Cell c;
+                    c = new Cell(posX, posY, original, solution);
+                    test.setCell(posX, posY, c);
                     break;
                 case 6:
-                    System.out.println(test.isOriginal());
-                    break;
-                case 7:
-                    System.out.println(test.isCorrect());
-                    break;
-                case 8:
-                    System.out.print("Valor a afegir:");
-                    test.addPossibleValue(sc.nextInt());
-                    break;
-                case 9:
-                    System.out.print("Valor a eliminar:");
-                    test.removePossibleValue(sc.nextInt());
-                    break;
-                case 10:
-                    System.out.println(test.getPossibleValues());
-                    break;
-                case 11:
-                    System.out.println(test.getSizePossibleValues());
+                    System.out.println("Introdueix posició X de la Cell:");
+                    posX = sc.nextInt();
+                    System.out.println("Introdueix posició Y de la Cell:");
+                    posY = sc.nextInt();
+                    c = test.getCell(posX, posY);
+                    System.out.println("Posició X = "+c.getPosX()+", posició Y = "+c.getPosY()+", valor correcte = "+c.getSolutionValue());
                     break;
             }
         }
