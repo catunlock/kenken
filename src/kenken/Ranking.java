dc50b1743f22ae750871a0753d63926103e88f47
Marc <Marc@W-Sobretaula>
2015-11-15 22:13:42 +0100

    ...

--- src/kenken/Ranking.java
+++ src/kenken/Ranking.java
@@ -5,13 +5,14 @@
  */
 package kenken;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  *
  * @author Marc Ferré Monné
  */
-public class Ranking {
+public class Ranking implements Serializable{
     
     public enum GameMode{Normal, TimeAttack};
     
