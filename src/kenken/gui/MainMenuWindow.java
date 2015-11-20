d9b688567c66d8fb3b86c0e80aaf67bbd4e939d1
Metainferno <marcf111@gmail.com>
2015-11-20 16:55:30 +0100

    Nomenclatura de botons i arxius canviada.

--- src/kenken/gui/MainMenuWindow.java
+++ src/kenken/gui/MainMenuWindow.java
@@ -30,17 +30,17 @@ public class MainMenuWindow extends javax.swing.JFrame {
         btnNewGame = new javax.swing.JButton();
         btnCreateBoard = new javax.swing.JButton();
         lblStatistics = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
+        pnlStatistics = new javax.swing.JPanel();
+        lblPlayedTime = new javax.swing.JLabel();
+        lblCreatedGames = new javax.swing.JLabel();
+        lblResolvedGames = new javax.swing.JLabel();
+        lblMainMenu = new javax.swing.JLabel();
+        btnLoadGame = new javax.swing.JButton();
+        btnOptions = new javax.swing.JButton();
+        btnExportBoard = new javax.swing.JButton();
+        btnImportBoard = new javax.swing.JButton();
+        btnShowRanking = new javax.swing.JButton();
+        btnLogOut = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -58,65 +58,65 @@ public class MainMenuWindow extends javax.swing.JFrame {
         lblStatistics.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
         lblStatistics.setText("User Statistics:");
 
-        jLabel2.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
-        jLabel2.setText("Total Time Played: ");
+        lblPlayedTime.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
+        lblPlayedTime.setText("Total Time Played: ");
 
-        jLabel3.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
-        jLabel3.setText("Games created: ");
+        lblCreatedGames.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
+        lblCreatedGames.setText("Games created: ");
 
-        jLabel4.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
-        jLabel4.setText("Games resolved:");
+        lblResolvedGames.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
+        lblResolvedGames.setText("Games resolved:");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout pnlStatisticsLayout = new javax.swing.GroupLayout(pnlStatistics);
+        pnlStatistics.setLayout(pnlStatisticsLayout);
+        pnlStatisticsLayout.setHorizontalGroup(
+            pnlStatisticsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlStatisticsLayout.createSequentialGroup()
                 .addGap(32, 32, 32)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel2))
+                .addGroup(pnlStatisticsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblResolvedGames)
+                    .addComponent(lblCreatedGames)
+                    .addComponent(lblPlayedTime))
                 .addContainerGap(199, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        pnlStatisticsLayout.setVerticalGroup(
+            pnlStatisticsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlStatisticsLayout.createSequentialGroup()
                 .addGap(24, 24, 24)
-                .addComponent(jLabel2)
+                .addComponent(lblPlayedTime)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel3)
+                .addComponent(lblCreatedGames)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel4)
+                .addComponent(lblResolvedGames)
                 .addContainerGap(40, Short.MAX_VALUE))
         );
 
-        jLabel5.setFont(new java.awt.Font("Flubber", 0, 48)); // NOI18N
-        jLabel5.setText("Welcome Back, <UserName>");
-        jLabel5.setToolTipText("");
+        lblMainMenu.setFont(new java.awt.Font("Flubber", 0, 48)); // NOI18N
+        lblMainMenu.setText("Welcome Back, <UserName>");
+        lblMainMenu.setToolTipText("");
 
-        jButton3.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
-        jButton3.setText("Load Game");
+        btnLoadGame.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
+        btnLoadGame.setText("Load Game");
 
-        jButton4.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
-        jButton4.setText("Options");
+        btnOptions.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
+        btnOptions.setText("Options");
 
-        jButton5.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
-        jButton5.setText("Export Board");
+        btnExportBoard.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
+        btnExportBoard.setText("Export Board");
 
-        jButton6.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
-        jButton6.setText("Import Board");
+        btnImportBoard.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
+        btnImportBoard.setText("Import Board");
 
-        jButton7.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
-        jButton7.setText("Show Ranking");
-        jButton7.addActionListener(new java.awt.event.ActionListener() {
+        btnShowRanking.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
+        btnShowRanking.setText("Show Ranking");
+        btnShowRanking.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton7ActionPerformed(evt);
+                btnShowRankingActionPerformed(evt);
             }
         });
 
-        jButton8.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
-        jButton8.setText("Logout");
+        btnLogOut.setFont(new java.awt.Font("Flubber", 0, 24)); // NOI18N
+        btnLogOut.setText("Logout");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -128,57 +128,57 @@ public class MainMenuWindow extends javax.swing.JFrame {
                         .addGap(103, 103, 103)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btnCreateBoard, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnLoadGame, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btnNewGame, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(btnImportBoard, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnExportBoard, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addComponent(lblStatistics)
                                 .addGap(138, 138, 138))
-                            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(pnlStatistics, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jButton7)
+                                .addComponent(btnShowRanking)
                                 .addGap(110, 110, 110))))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap(174, Short.MAX_VALUE)
-                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 666, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(lblMainMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 666, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(61, 61, 61))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnOptions, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnLogOut, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(13, 13, 13))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(44, 44, 44)
-                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lblMainMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblStatistics)
                         .addGap(18, 18, 18)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(pnlStatistics, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(btnShowRanking, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnNewGame, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnLoadGame, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnCreateBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnImportBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnExportBoard, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(39, 39, 39)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnOptions, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnLogOut, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
@@ -189,9 +189,9 @@ public class MainMenuWindow extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btnNewGameActionPerformed
 
-    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+    private void btnShowRankingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnShowRankingActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton7ActionPerformed
+    }//GEN-LAST:event_btnShowRankingActionPerformed
 
     /**
      * @param args the command line arguments
@@ -230,18 +230,18 @@ public class MainMenuWindow extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCreateBoard;
+    private javax.swing.JButton btnExportBoard;
+    private javax.swing.JButton btnImportBoard;
+    private javax.swing.JButton btnLoadGame;
+    private javax.swing.JButton btnLogOut;
     private javax.swing.JButton btnNewGame;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton btnOptions;
+    private javax.swing.JButton btnShowRanking;
+    private javax.swing.JLabel lblCreatedGames;
+    private javax.swing.JLabel lblMainMenu;
+    private javax.swing.JLabel lblPlayedTime;
+    private javax.swing.JLabel lblResolvedGames;
     private javax.swing.JLabel lblStatistics;
+    private javax.swing.JPanel pnlStatistics;
     // End of variables declaration//GEN-END:variables
 }
