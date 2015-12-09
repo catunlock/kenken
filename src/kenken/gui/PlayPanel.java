9bcdb224fbee76e31b5b18412d31489a741e08af
asus <asus@asuspc>
2015-12-09 16:51:28 +0100

    boton de surrender quitado de PlayPanel

--- src/kenken/gui/PlayPanel.java
+++ src/kenken/gui/PlayPanel.java
@@ -69,7 +69,6 @@ public class PlayPanel extends javax.swing.JPanel {
         btnMusic = new javax.swing.JButton();
         lblTime = new javax.swing.JLabel();
         btnExit = new javax.swing.JButton();
-        btnSurrender = new javax.swing.JButton();
         btnSaveGame = new javax.swing.JButton();
         btnHint = new javax.swing.JButton();
         lblHint = new javax.swing.JLabel();
@@ -93,20 +92,6 @@ public class PlayPanel extends javax.swing.JPanel {
         btnExit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnExitActionPerformed(evt);
-            }
-        });
-
-        btnSurrender.setFont(new java.awt.Font("Flubber", 0, 18)); // NOI18N
-        btnSurrender.setText("SURRENDER");
-        btnSurrender.setToolTipText("");
-        btnSurrender.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnSurrenderMouseClicked(evt);
-            }
-        });
-        btnSurrender.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSurrenderActionPerformed(evt);
             }
         });
 
@@ -150,13 +135,6 @@ public class PlayPanel extends javax.swing.JPanel {
                 .addGap(25, 25, 25)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnSaveGame)
-                        .addGap(178, 178, 178)
-                        .addComponent(btnSurrender)
-                        .addGap(21, 21, 21))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(btnHint, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(lblHint)
@@ -164,7 +142,12 @@ public class PlayPanel extends javax.swing.JPanel {
                         .addComponent(btnMusic)
                         .addGap(18, 18, 18)
                         .addComponent(lblTime, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(42, 42, 42))))
+                        .addGap(42, 42, 42))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnSaveGame)
+                        .addGap(24, 24, 24))))
             .addGroup(layout.createSequentialGroup()
                 .addGap(189, 189, 189)
                 .addComponent(boardPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 418, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -188,7 +171,6 @@ public class PlayPanel extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnSurrender, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnSaveGame, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(21, 21, 21))
         );
@@ -214,9 +196,6 @@ public class PlayPanel extends javax.swing.JPanel {
         
     }//GEN-LAST:event_btnExitActionPerformed
 
-    private void btnSurrenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSurrenderActionPerformed
-    }//GEN-LAST:event_btnSurrenderActionPerformed
-
     private void btnSaveGameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveGameActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnSaveGameActionPerformed
@@ -224,13 +203,6 @@ public class PlayPanel extends javax.swing.JPanel {
     private void btnHintActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHintActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnHintActionPerformed
-
-    private void btnSurrenderMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSurrenderMouseClicked
-        ((EndGamePanel) mw.getPanel(MainWindow.Panels.EndGamePanel)).setBoardPlayed("Manolito");
-        mw.setPanel(MainWindow.Panels.EndGamePanel);
-        lblTime.setText("00:00:00");
-        timer.stop();
-    }//GEN-LAST:event_btnSurrenderMouseClicked
     
     
     
@@ -271,7 +243,6 @@ public class PlayPanel extends javax.swing.JPanel {
     private javax.swing.JButton btnHint;
     private javax.swing.JButton btnMusic;
     private javax.swing.JButton btnSaveGame;
-    private javax.swing.JButton btnSurrender;
     private javax.swing.JLabel lblHint;
     private javax.swing.JLabel lblTime;
     // End of variables declaration//GEN-END:variables
