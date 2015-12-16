6f1eabf5ba8159f0b6085f2b09f6d94c0a255d7c
Metainferno <marcf111@gmail.com>
2015-12-16 02:45:50 +0100

    Documentación lista.

--- src/kenken/domain/controllers/UserControllerKenken.java
+++ src/kenken/domain/controllers/UserControllerKenken.java
@@ -13,23 +13,40 @@ import java.time.Duration;
  */
 public class UserControllerKenken extends UserController{
     
+    /**
+     * Get the users name.
+     * @return A string with the name of the user.
+     */
     public String getUsername() {
         if (this.getLoggedUser() == null) return "";
         else return this.getLoggedUser().getUsername();
     }
     
+    /**
+     * Increments the started games.
+     */
     public void incrementStartedGames(){
         this.getLoggedUser().incrementStartedGames();
     }
     
+    /**
+     * Increments the solved games.
+     */
     public void incrementSolvedGames() {
         this.getLoggedUser().incrementSolvedGames();
     }
     
+    /**
+     * Increments the total Created Boards.
+     */
     public void incrementTotalCreatedBoards(){
         this.getLoggedUser().incrementTotalCreatedBoards();
     }
     
+    /**
+     * Sums loggeduser.time and time.
+     * @param time The time to be operated.
+     */
     public void incrementTime(Duration time){
         this.getLoggedUser().incrementTotalTimePlayed(time);
     }
